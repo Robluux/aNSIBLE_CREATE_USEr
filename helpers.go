6cdfe0b39d98a9403f5b76fa6cad694dcd6cbdf1
Robluux <97086018+Robluux@users.noreply.github.com>
2023-04-04 15:22:45 +0000

    Brazilian portuguese translation update.

--- helpers.go
+++ helpers.go
@@ -26,3 +26,4 @@ func (c Client) Get() (ResponsePayload, error) {
 	resp, err := c.Client.Get(fmt.Sprintf("%s?function=%s&symbol=%s&outputsize=%s&apikey=%s", APIURL, c.Options.Function, c.Options.Symbol, c.Options.OutputSize, c.Options.APIKey))
 	if err != nil {
 		return rpload, err
+	}
