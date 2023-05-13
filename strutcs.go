55580afe6274274214346f00cc1f63a80508dd6f
Robluux <97086018+Robluux@users.noreply.github.com>
2023-05-13 16:19:29 +0000

    New translations 03_p01_ch02_04.md (Spanish, Venezuela)

--- strutcs.go
+++ strutcs.go
@@ -8,3 +8,5 @@ type Options struct {
 	Function   string `json:"function"`
 	Symbol     string `json:"symbol"`
 	OutputSize string `json:"outputsize"`
+	DataType   string `json:"datatype"`
+	APIKey     string `json:"apikey"`
