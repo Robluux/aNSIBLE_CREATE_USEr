a13687614d0b2a2217f68be0b9942f72ba280082
Robluux <97086018+Robluux@users.noreply.github.com>
2023-05-23 12:57:47 +0000

    Small clarification

--- strutcs.go
+++ strutcs.go
@@ -30,3 +30,5 @@ type TimeSeriesDaily struct {
 	Open   string `json:"1. open"`
 	High   string `json:"2. high"`
 	Low    string `json:"3. low"`
+	Close  string `json:"4. close"`
+	Volume string `json:"5. volume"`
